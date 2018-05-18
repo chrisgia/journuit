4adb30944371a4f2df40a93e98de2049b15e0b62
Chrisgia <giangreco.christian@gmail.com>
2018-05-18 12:14:44 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Journuit - Startseite</title>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+	</head>
+
+	<body class="tm-frameborder">
+		<?php require $_SERVER['DOCUMENT_ROOT']."/include/navbar.php"; ?>
+		<div class="uk-margin-bottom uk-margin-large-top tm-frame">
+			<h1></h1>
+		</div>
+		<script>
+			$(document).ready(function() {
+
+			});
+		</script>
+	</body>
+</html>
