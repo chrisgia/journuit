038524021a255a2fe47a2d29d4553a81ec0086ed
Chrisgia <giangreco.christian@gmail.com>
2018-05-18 16:05:30 +0200

    erstellen der Navbar

--- index.php
+++ index.php
@@ -5,19 +5,28 @@
 		<meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/uikit.min.css" />
+        <link rel="stylesheet" href="css/fontawesome-all.css" />
+        <link rel="stylesheet" href="css/custom.css" />
         <script src="js/uikit.min.js"></script>
         <script src="js/uikit-icons.min.js"></script>
 	</head>
 
-	<body class="tm-frameborder">
-		<?php require $_SERVER['DOCUMENT_ROOT']."/include/navbar.php"; ?>
-		<div class="uk-margin-bottom uk-margin-large-top tm-frame">
-			<h1>Journuit - Startseite</h1>
-		</div>
-		<script>
-			$(document).ready(function() {
-
-			});
-		</script>
+	<body>
+		<nav class="uk-navbar-container uk-height-1-1" id="navbar" uk-navbar>
+			<div class="uk-navbar-left">
+				<div class="uk-navbar-item">
+		        	<a class="uk-link-heading" href="index.php/register.php">REGISTRIEREN</a>
+		        </div>
+		        <div class="uk-navbar-item">
+		        	<img src="pictures/dot.png" alt="dot" uk-img">
+		    	</div>
+		    	<div class="uk-navbar-item">
+		        	<a class="uk-link-heading" href="index.php/login.php">ANMELDEN</a>
+		    	</div>
+		    </div>
+		    <div class="uk-navbar-right">
+		        <a class="uk-navbar-item uk-logo" href="index.php"><span id="jour">jour</span><span id="nuit">nuit</span> <img data-src="pictures/journuit-logo_mini.png" alt="journuit Logo" uk-img></a>
+		    </div>
+		</nav>
 	</body>
 </html>
