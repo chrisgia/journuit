59f4fc1b4f39a93e93d40f9eab1428deff897b21
Chrisgia <giangreco.christian@gmail.com>
2018-05-23 09:38:01 +0200

    Startseite für Desktop fertig

--- index.php
+++ index.php
@@ -34,19 +34,29 @@
 
 		<div id="banner" class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover" data-src="pictures/sunset-1920w.png" data-srcset="pictures/sunset-375w.png 375w, pictures/sunset-1920w.png 1920w" uk-img>
 			<h1 id="slogan" class="uk-text-center uk-margin-large-top">Verewige deine Errinerungen. <br/><span id="white">Tag</span> und Nacht.</h1>
-			<h1 id="slogan" class="uk-text-top uk-margin-large-top uk-visible@s">Verewige deine Errinerungen. <br/><span id="white">Tag</span> und Nacht.</h1>
+			<!-- <h1 id="slogan" class="uk-text-top uk-margin-large-top uk-hidden@m">Verewige deine Errinerungen. <br/><span id="white">Tag</span> und Nacht.</h1> -->
 		</div>
 
-		<div class="uk-container uk-container-expand uk-height-match">
+		<div class="uk-container uk-container-large">
 			<div uk-grid class="uk-flex-center uk-flex-middle">
-				<div class="uk-width-1-4">
-					<img data-src="pictures/devices_mockup.png" alt="devices" uk-img>
+				<div uk-grid class="uk-flex-center uk-flex-middle">
+					<div class="uk-width-1-4 uk-flex-first">
+						<img data-src="pictures/devices_mockup.png" alt="devices" uk-img>
+					</div>
+					<div class="uk-width-1-2 uk-flex-last">
+						<span id="beschreibung">Mit journuit können Sie jederzeit und von jedem Gerät aus Ihre schönsten Momente festhalten.</span>
+					</div>
 				</div>
-				<div class="uk-width-1-2">
-					<span id="beschreibung">Mit journuit können Sie jederzeit und von jedem Gerät aus Ihre schönsten Momente festhalten.</span>
+				<hr class="uk-width-1-1 uk-divider-icon">
+				<div uk-grid class="uk-flex-center uk-flex-middle">
+					<div class="uk-width-1-2 uk-flex-first">
+						<a href="http://www.euresa-reisen.de" target="_blank"><img data-src="pictures/logo-er.png" alt="EURESA Logo" uk-img></a>
+					</div>
+					<div class="uk-width-1-2 uk-flex-last">
+						<a href="https://www.dfhi-isfates.eu/de/" target="_blank"><img data-src="pictures/logo-dfhi_isfates.png" alt="DFHI/ISFATES Logo" uk-img></a>
+					</div>
 				</div>
 			</div>
 		</div>
-
 	</body>
 </html>
